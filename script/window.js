dc533595af8c4ae4157798a1df21fe62e011f501
Fire-Witch <74001300+Fire-Witch@users.noreply.github.com>
2020-11-07 15:56:32 +0100

    update window func

--- script/window.js
+++ script/window.js
@@ -1,8 +1,18 @@
-function openWindowTwitch(){
-    document.getElementById("windowPlace").innerHTML += '';
-}
-
+//général
 function closeWindow(id){
     var element = document. getElementById(id);
     element.parentNode.removeChild(element);
+}
+
+function deplaceWindow(id){
+    //on récupère la position X Y de la souris
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+    //on récupère la postion de la fenètre
+    var posWindow = document.getElementById(id).getBoundingClientRect();
+}
+
+//twitch
+function openWindowTwitch(){
+    document.getElementById("windowPlace").innerHTML += '';
 }
